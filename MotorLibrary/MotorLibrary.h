71a0245dcae5a77efe7610ad828eeef2e0dae53b
faroga85 <60296933+faroga85@users.noreply.github.com>
2020-04-10 00:44:21 +0430

    init .h file

--- MotorLibrary/MotorLibrary.h
+++ MotorLibrary/MotorLibrary.h
@@ -17,6 +17,18 @@ class AX_RX_XL
     void change_id_XL(unsigned char new_id);
     void change_baudrate_XL(unsigned char baudrate_code);
     void return_level_XL(char level);
+    ////
+    void return_level(char level);
+    void wheel(char ccw_cw, int speed);
+    void change_baudrate(char baudrate_code);
+    void change_id(char new_id);
+    void torque(char State);
+    void led(char State);
+    void joint(unsigned int Position, unsigned int Speed, unsigned int Time);
+    void speed(unsigned int Speed);
+    void angle_limit(unsigned int ccw_limit, unsigned int cw_limit);
+    void goto_joint_mode();
+    void goto_wheel_mode();
   private:
     unsigned short update_crc_XL(unsigned short crc_accum, unsigned char *data_blk_ptr, unsigned short data_blk_size);
 };
